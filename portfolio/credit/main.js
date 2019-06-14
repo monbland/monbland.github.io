0e73d030ed62727806e6879b4a58a2619437137f
Moonbland <moonbland@MacBook-m.local>
2019-06-14 18:04:21 +0300

    #zoom chart and change bio on index

--- portfolio/credit/main.js
+++ portfolio/credit/main.js
@@ -1,10 +1,26 @@
+var ONEDAY = 86400000;
+var myChart = echarts.init(document.getElementById('main-chart'));
 var button = document.getElementById("button-go");
+var profitCount = [];
+var startDay
+// class DayProfit {
+//     constructor(num, str, profit) {
+//         this.num = num;
+//         this.str = str;
+//         this.profit = profit;
+//     }
+// }
 button.onclick = function() {
     class WithdrawalTimer {
         constructor(timer, withdrawal) {
             this.timer = timer;
             this.withdrawal = withdrawal;
         }
+    }
+    var dayCount = {
+        num: [],
+        str: [],
+        profit: []
     }
     var creditLimit = +document.getElementById("credit-limit").value;
     var oneTimeWithdrawal = +document.getElementById("one-time-withdrawal").value;
@@ -13,6 +29,7 @@ button.onclick = function() {
     var debetPercent = +document.getElementById("debet-percent").value / 36500;
     var days = +document.getElementById("days-range").value;
     var dayStart = moment(document.getElementById("day-start").value);
+    startDay = new Date(document.getElementById("day-start").value).valueOf();
     if (dayStart._isValid == false) {
         document.getElementById('day-start').value = moment().format("YYYY-MM-DD");
         dayStart = moment();
@@ -20,16 +37,16 @@ button.onclick = function() {
     var debetBalance = 0;
     var withdrawalTimers = new WithdrawalTimer;
     withdrawalTimers = [];
-    var profitCount = [];
-    var dayCount = [];
-    profitCount[0] = -annualMaintenance;
-    var dayCountMoment = []
-    dayCountMoment[0] = dayStart;
-    dayCount[0] = dayStart.format('D.M.YY');
+    dayCount.num.push(0);
+    dayCount.str.push(dayStart.format('YYYY-MM-DD'));
+    dayCount.profit.push(-annualMaintenance);
+    // dayCount[0].profit = -annualMaintenance;
+    // dayCount[0].str = dayStart.format('YYYY-MM-DD');
+    // dayCount[0].num = 0;
     var creditTimer = 0;
     var profitDay = -2;
     var profitDayProfit = 0;
-    var bool = true;
+    let bool = true;
     for(var day = 0; day < days; day++) {
         if (creditTimer == 0) {
             switch (true) {
@@ -55,23 +72,22 @@ button.onclick = function() {
             debetBalance -= withdrawalTimers[0].withdrawal;
             withdrawalTimers.shift({});
         };
-        profitCount[day + 1] = Math.round((profitCount[day] +  debetBalance * debetPercent) * 100) / 100;
-        dayCountMoment[day+1] = dayCountMoment[day].add(1,'d');
-        dayCount[day + 1] = dayCountMoment[day+1].format('D.M.YY');
+        dayCount.num.push(day + 1);
+        dayCount.str.push(moment(dayCount.str[day]).add(1, 'd').format("YYYY-MM-DD"));
+        dayCount.profit.push(Math.round((dayCount.profit[day] + debetBalance * debetPercent) * 100) / 100);
         for (var i = 0; i < withdrawalTimers.length; i++) {
             withdrawalTimers[i].timer--;
         };
         creditTimer--;
-        if (profitCount[day+1]>0 && bool) {
+        if (dayCount.profit[day+1]>0 && bool) {
             profitDay = day+1;
-            profitDayProfit = profitCount[day+1];
+            profitDayProfit = dayCount.profit[day+1];
             bool = false;
         };
         if (day%365==0 && day!=0) {
-            profitCount[day+1] -= annualMaintenance;
+            dayCount.profit[day+1] -= annualMaintenance;
         };
     }
-    var myChart = echarts.init(document.getElementById('main-chart'));
     option = {
         title: {
             text: 'График прибыли'
@@ -96,18 +112,31 @@ button.onclick = function() {
                 }
             }
         },
+        dataZoom: [{
+            id: 'dataZoomX',
+            type: 'slider',
+            xAxisIndex: [0],
+            filterMode: 'filter'
+        }],
         xAxis: {
             type: 'category',
-            data: dayCount
+            data: dayCount.num,
+            axisLabel: {
+                formatter: function (value, index) {
+                    // Formatted to be month/day; display year only in the first label
+                    var label = dayCount.str[value];
+                    return label;
+                }
+            }
         },
         yAxis: {
-            type: 'value'
+            type: 'value',
         },
         series: [{
             name: 'доход',
             type: 'line',
+            data: dayCount.profit,
             smooth: true,
-            data: profitCount,
             markPoint: {
                 data: [
                     {name: 'бинго!', value: 'бинго!', yAxis: profitDayProfit, xAxis: profitDay}
@@ -117,5 +146,30 @@ button.onclick = function() {
     };
     myChart.setOption(option);
     document.getElementById("day-profit").value = profitDay;
-    document.getElementById("profit").value = profitCount[days];
+    document.getElementById("profit").value = dayCount.profit[days];
+}
+document.getElementById("button-range-zoom").onclick = function () {
+    var dateStart, dateEnd;
+    dateStart = new Date(document.getElementById("days-range-start").value).valueOf();
+    dateEnd = new Date(document.getElementById("days-range-end").value).valueOf();
+    let bool = document.getElementById("days-range-start").value!=""&&document.getElementById("days-range-end").value!="";
+    switch (true) {
+        case bool&&dateStart<=dateEnd:
+            myChart.dispatchAction({
+                type: 'dataZoom',
+                startValue: (dateStart - startDay) / ONEDAY,
+                endValue: (dateEnd - startDay) / ONEDAY
+            });
+            break;
+        case bool&&dateStart>dateEnd:
+            myChart.dispatchAction({
+                type: 'dataZoom',
+                startValue: (dateEnd - startDay) / ONEDAY,
+                endValue: (dateStart - startDay) / ONEDAY
+            });
+            break;
+        default:
+            alert("Одна из дат не введена или введена неправильно!");
+            break;
+    };
 }
