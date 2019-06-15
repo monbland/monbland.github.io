e1f035e7481e864e51d167edbc4bb07fe37cb7ea
Moonbland <moonbland@MacBook-m.local>
2019-06-15 21:15:56 +0300

    #sirius fix1

--- portfolio/sirius-landing/main.js
+++ portfolio/sirius-landing/main.js
@@ -1,22 +1,20 @@
-(function () {
-    var $pic = $(".pic");
-    $pic.on('mousemove', function(e) {
-        var w, h, offsetPic, offsetX, offsetY, transformPic;
-        var elem = document.getElementsByClassName('pic');
-        var picPosition = elem[0].getBoundingClientRect();
-        w = picPosition.right - picPosition.left;
-        h = picPosition.bottom - picPosition.top;
-        offsetX = 0.5 - (e.screenX - picPosition.left) / w;
-        offsetY = 0.5 - (e.screenY - picPosition.top) / h;
-        offsetPic = $pic.data('offset');
-        transformPic = 'translateY(' + -offsetX * offsetPic + 'px) rotateX(' + -offsetY * offsetPic + 'deg) rotateY(' + offsetX * (offsetPic * 2) + 'deg)';
-        $pic.css('transform', transformPic);
-    });
-    $pic.on('mouseout', function(e) {
-        var transformPic = 'translateY(0px) rotateX(0deg) rotateY(0deg)';
-        $pic.css('transform', transformPic);
-    });
-}).call(this);
+console.log(document.getElementById("picture"));
+var $pic = $(".pic")
+$pic.on("mousemove", function(e) {
+    var w, h, offsetPic, offsetX, offsetY, transformPic;
+    var picPosition = document.getElementById("picture").getBoundingClientRect();
+    w = picPosition.right - picPosition.left;
+    h = picPosition.bottom - picPosition.top;
+    offsetX = 0.5 - (e.screenX - picPosition.left) / w;
+    offsetY = 0.5 - (e.screenY - picPosition.top) / h;
+    offsetPic = $pic.data('offset');
+    transformPic = 'translateY(' + -offsetX * offsetPic + 'px) rotateX(' + -offsetY * offsetPic + 'deg) rotateY(' + offsetX * (offsetPic * 2) + 'deg)';
+    $pic.css('transform', transformPic);
+});
+$pic.on('mouseout', function(e) {
+    var transformPic = 'translateY(0px) rotateX(0deg) rotateY(0deg)';
+    $pic.css('transform', transformPic);
+});
 $('a[href^="#"]').on('click',function (e) {
     e.preventDefault();
     var id = $(this).attr('href'),
