73cf19bcf10f236e3f4e4ba09704d77fabbb3e23
Moonbland <moonbland@MacBook-m.local>
2019-06-16 21:19:20 +0300

    #sirius fix2

--- portfolio/sirius-landing/main.js
+++ portfolio/sirius-landing/main.js
@@ -3,13 +3,14 @@ var $pic = $(".pic")
 $pic.on("mousemove", function(e) {
     var w, h, offsetPic, offsetX, offsetY, transformPic;
     var picPosition = document.getElementById("picture").getBoundingClientRect();
-    w = picPosition.right - picPosition.left;
-    h = picPosition.bottom - picPosition.top;
-    offsetX = 0.5 - (e.screenX - picPosition.left) / w;
-    offsetY = 0.5 - (e.screenY - picPosition.top) / h;
+    w = e.currentTarget.width;
+    h = e.currentTarget.height;
+    offsetX = 0.5 - (e.pageX - $pic.offset().left) / w;
+    offsetY = 0.5 - (e.pageY - $pic.offset().top) / h;
     offsetPic = $pic.data('offset');
-    transformPic = 'translateY(' + -offsetX * offsetPic + 'px) rotateX(' + -offsetY * offsetPic + 'deg) rotateY(' + offsetX * (offsetPic * 2) + 'deg)';
+    transformPic = 'rotateX(' + -offsetY * offsetPic + 'deg) rotateY(' + offsetX * (offsetPic * 2) + 'deg)';
     $pic.css('transform', transformPic);
+    console.log(e.screenY + " " + e.currentTarget.width+ "  " + h);
 });
 $pic.on('mouseout', function(e) {
     var transformPic = 'translateY(0px) rotateX(0deg) rotateY(0deg)';
